501a7f6a26e4541d8b8247d7bad23e86f840f3ec
Karnikhil90 <nikhilkarmakar4020@gmail.com>
2024-05-25 10:03:22 +0530

    initil add program.very basic stuff........

---
+++ basic/main.rs
@@ -0,0 +1,8 @@
+
+fn main(){
+    let name = "Nikhil Karmakar";
+    let a = 35;
+    let b = 65;
+    println!("My name is {}",name);
+    println!("Answer is {}", (a+b))
+}
---
+++ calculations/main.rs
@@ -0,0 +1,9 @@
+fn main(){
+    let num = 56;
+    let num2 = 33;
+
+    println!("The sum is {}",(num+num2));
+    println!("The sub is {}",(num-num2));
+    println!("The product is {}",(num*num2));
+    println!("The div is {}",(num/num2));
+}
